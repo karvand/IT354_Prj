8154a79780e1734f8e0bc4f603660be623ec9e8b
karvand <kaveharvand@gmail.com>
2015-05-01 18:03:51 -0500

    js code commented, .apk added, more documentation
    myScript.js cleaned and commented, apk file built and .docx file
    completed

--- www/js/myScript.js
+++ www/js/myScript.js
@@ -1,12 +1,17 @@
+//Function to get the list of movie search results based on a given keyword.
 function getMovies() {
-	xmlhttp = new XMLHttpRequest();
+	xmlhttp = new XMLHttpRequest(); 
 	var movieTitle = document.getElementById("title").value;
+//Check to see if a keyword is inserted or not
 	if (movieTitle == "") {
 		document.getElementById("output1").innerHTML = "Please enter name of the movie";
-	} else {
+	} 
+	else 
+	{
 		document.getElementById("output1").innerHTML = "";
 		element = document.getElementById("output2");
 		element.innerHTML = "";
+		//transition to page2
 		$.mobile.changePage($("#page2"), {
 			transition: "slide",
 		});
@@ -14,10 +19,12 @@ function getMovies() {
 		var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xwd6czu67s5vbdy2tpz5eyhp" + "&q=" + encodeURI(movieTitle);
 		xmlhttp.open('GET', url, true);
 		xmlhttp.setRequestHeader("Accept", "application/json");
+		//sending the request to RESTful web service
 		xmlhttp.send(null);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
 				if (xmlhttp.status == 200) {
+					//parsing the json response 
 					var movieInfo = JSON.parse(xmlhttp.responseText);
 					if (movieInfo.total != 0) {
 						element.innerHTML = movieInfo.movies.length + " movie(s) found";
@@ -26,7 +33,21 @@ function getMovies() {
 							for (j = 0; j < movieInfo.movies[i].abridged_cast.length; j++) {
 								cast = cast + movieInfo.movies[i].abridged_cast[j].name + ", ";
 							}
-							element.innerHTML = element.innerHTML + "<br/>" + "<img src=" + movieInfo.movies[i].posters.thumbnail + ">" + "<h3>" + movieInfo.movies[i].title + "(" + movieInfo.movies[i].year + ")" + "</h3>" + "<b>Rating: </b>" + movieInfo.movies[i].mpaa_rating + "<br/>" + "<b>Runtime: </b>" + movieInfo.movies[i].runtime + " min<br/>" + "<b>Release Dates: </b><ul><li><b>In Theaters: </b>" + movieInfo.movies[i].release_dates.theater + "</li><li><b>On DVD: </b>" + movieInfo.movies[i].release_dates.dvd + "</li></ul>" + "<b>Critics Rating: </b>" + movieInfo.movies[i].ratings.critics_rating + "<br/>" + "<b>Critics Score: </b>" + movieInfo.movies[i].ratings.critics_score + "<br/>" + "<b>Audience Rating: </b>" + movieInfo.movies[i].ratings.audience_rating + "<br/>" + "<b>Audience Score: </b>" + movieInfo.movies[i].ratings.audience_score + "<br/>" + "<b>Summary: </b>" + movieInfo.movies[i].synopsis + "<br/>" + "<b>Cast: </b>" + cast + "<br/><br/>" + "<a onclick='getReviews(/" + movieInfo.movies[i].id + "/,/" + movieInfo.movies[i].title + "/)' href='#'><b>Reviews</b></a>" + "<br/><br/><hr>";
+							element.innerHTML = element.innerHTML + "<br/>" + "<img src=" 
+							+ movieInfo.movies[i].posters.thumbnail + ">" + "<h3>"
+							+ movieInfo.movies[i].title + "(" + movieInfo.movies[i].year + ")" 
+							+ "</h3>" + "<b>Rating: </b>" + movieInfo.movies[i].mpaa_rating
+							+ "<br/>" + "<b>Runtime: </b>" + movieInfo.movies[i].runtime + " min<br/>" 
+							+ "<b>Release Dates: </b><ul><li><b>In Theaters: </b>" 
+							+ movieInfo.movies[i].release_dates.theater + "</li><li><b>On DVD: </b>" 
+							+ movieInfo.movies[i].release_dates.dvd + "</li></ul>" + "<b>Critics Rating: </b>"
+							+ movieInfo.movies[i].ratings.critics_rating + "<br/>" + "<b>Critics Score: </b>" 
+							+ movieInfo.movies[i].ratings.critics_score + "<br/>" + "<b>Audience Rating: </b>"
+							+ movieInfo.movies[i].ratings.audience_rating + "<br/>" + "<b>Audience Score: </b>"
+							+ movieInfo.movies[i].ratings.audience_score + "<br/>" + "<b>Summary: </b>" 
+							+ movieInfo.movies[i].synopsis + "<br/>" + "<b>Cast: </b>" + cast + "<br/><br/>" 
+							+ "<a onclick='getReviews(/" + movieInfo.movies[i].id + "/,/"+ movieInfo.movies[i].title 
+							+"/)' href='#'><b>Reviews</b></a>" + "<br/><br/><hr>";
 						}
 					} else element.innerHTML = "No movies found";
 				} else alert("Error -> xmlhttp status: " + xmlhttp.status);
@@ -35,12 +56,14 @@ function getMovies() {
 	}
 }
 
+//Function to show the reviews about a particular movie
 function getReviews(movieId, movieTitle) {
 	var name = String(movieTitle);
 	var movieName = name.substring(1, name.length - 1);
 	xmlhttp = new XMLHttpRequest();
 	element = document.getElementById("output3");
 	element.innerHTML = "";
+	//transition to page3
 	$.mobile.changePage($("#page3"), {
 		transition: "slide",
 	});
@@ -48,15 +71,18 @@ function getReviews(movieId, movieTitle) {
 	var url = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieId + "/reviews.json?apikey=" + key + "&review_type=top_critic";
 	xmlhttp.open('GET', url, true);
 	xmlhttp.setRequestHeader("Accept", "application/json");
+	//Sending the request to RESTfull web service
 	xmlhttp.send(null);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
 			if (xmlhttp.status == 200) {
+				//parsing the json response
 				var reviewInfo = JSON.parse(xmlhttp.responseText);
 				if (reviewInfo.reviews.length != 0) {
 					element.innerHTML = "<h3>Reviews for " + movieName + " :</h3>";
 					for (i = 0; i < reviewInfo.reviews.length; i++) {
-						element.innerHTML = element.innerHTML + reviewInfo.reviews[i].quote + " " + reviewInfo.reviews[i].critic + ", <i>" + reviewInfo.reviews[i].publication + "</i>, " + reviewInfo.reviews[i].date + "<br/></br>";
+						element.innerHTML = element.innerHTML + reviewInfo.reviews[i].quote + " " + reviewInfo.reviews[i].critic + ", <i>" 
+						+ reviewInfo.reviews[i].publication + "</i>, " + reviewInfo.reviews[i].date + "<br/></br>";
 					}
 				} else element.innerHTML = "No reviews found";
 			} else alert("Error -> xmlhttp status: " + xmlhttp.status);
@@ -64,6 +90,7 @@ function getReviews(movieId, movieTitle) {
 	};
 }
 
+//Function to retrieve information about the top ten movies in the box office
 function getBoxOffice() {
 	xmlhttp = new XMLHttpRequest();
 	element = document.getElementById("output4");
@@ -72,10 +99,12 @@ function getBoxOffice() {
 	var url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=" + key;
 	xmlhttp.open('GET', url, true);
 	xmlhttp.setRequestHeader("Accept", "application/json");
+	//Sending the request to the RESTful web service
 	xmlhttp.send(null);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
 			if (xmlhttp.status == 200) {
+				//parsing the json response
 				var movieInfo = JSON.parse(xmlhttp.responseText);
 				if (movieInfo.movies.length != 0) {
 					for (i = 0; i < movieInfo.movies.length; i++) {
@@ -84,7 +113,15 @@ function getBoxOffice() {
 						for (j = 0; j < movieInfo.movies[i].abridged_cast.length; j++) {
 							cast = cast + movieInfo.movies[i].abridged_cast[j].name + ", ";
 						}
-						element.innerHTML = element.innerHTML + "<img src=" + movieInfo.movies[i].posters.thumbnail + ">" + "<h3>" + count + "- " + movieInfo.movies[i].title + "</h3>" + "<b>Rating: </b>" + movieInfo.movies[i].mpaa_rating + "<br/>" + "<b>Runtime: </b>" + movieInfo.movies[i].runtime + " min<br/>" + "<b>Release Dates: </b>" + movieInfo.movies[i].release_dates.theater + "<br/>" + "<b>Critics Rating: </b>" + movieInfo.movies[i].ratings.critics_rating + "<br/>" + "<b>Critics Score: </b>" + movieInfo.movies[i].ratings.critics_score + "<br/>" + "<b>Audience Rating: </b>" + movieInfo.movies[i].ratings.audience_rating + "<br/>" + "<b>Audience Score: </b>" + movieInfo.movies[i].ratings.audience_score + "<br/>" + "<b>Summary: </b>" + movieInfo.movies[i].synopsis + "<br/>" + "<b>Cast: </b>" + cast + "<hr>";
+						element.innerHTML = element.innerHTML + "<img src=" + movieInfo.movies[i].posters.thumbnail 
+						+ ">" + "<h3>" + count + "- " + movieInfo.movies[i].title + "</h3>" + "<b>Rating: </b>"
+						+ movieInfo.movies[i].mpaa_rating + "<br/>" + "<b>Runtime: </b>" + movieInfo.movies[i].runtime 
+						+ " min<br/>" + "<b>Release Dates: </b>" + movieInfo.movies[i].release_dates.theater + "<br/>" 
+						+ "<b>Critics Rating: </b>" + movieInfo.movies[i].ratings.critics_rating + "<br/>" 
+						+ "<b>Critics Score: </b>" + movieInfo.movies[i].ratings.critics_score + "<br/>" 
+						+ "<b>Audience Rating: </b>" + movieInfo.movies[i].ratings.audience_rating + "<br/>"
+						+ "<b>Audience Score: </b>" + movieInfo.movies[i].ratings.audience_score + "<br/>"
+						+ "<b>Summary: </b>" + movieInfo.movies[i].synopsis + "<br/>" + "<b>Cast: </b>" + cast + "<hr>";
 					}
 				} else element.innerHTML = "No movies found";
 			} else alert("Error -> xmlhttp status: " + xmlhttp.status);
@@ -92,6 +129,7 @@ function getBoxOffice() {
 	};
 }
 
+//Function to get the thumbnails of top 10 box office movies
 function getBoxOfficeImages() {
 	xmlhttp = new XMLHttpRequest();
 	element = document.getElementById("images");
@@ -99,14 +137,18 @@ function getBoxOfficeImages() {
 	var url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=" + key;
 	xmlhttp.open('GET', url, true);
 	xmlhttp.setRequestHeader("Accept", "application/json");
+	//Sending the request to RESTful web service
 	xmlhttp.send(null);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
 			if (xmlhttp.status == 200) {
+				//Parsing the json response
 				var movieInfo = JSON.parse(xmlhttp.responseText);
 				if (movieInfo.movies.length != 0) {
 					for (i = 0; i < movieInfo.movies.length; i++) {
-						element.innerHTML += "<a onclick='getMovieDetailsFromImages(\"" + movieInfo.movies[i].id + "\")' href='#'><img id='image' src = " + movieInfo.movies[i].posters.thumbnail + "></a>";
+						element.innerHTML += "<a onclick='getMovieDetailsFromImages(\"" 
+						+ movieInfo.movies[i].id + "\")' href='#'><img id='image' src = "
+						+ movieInfo.movies[i].posters.thumbnail + "></a>";
 					}
 				} else alert("Error -> xmlhttp status: " + xmlhttp.status);
 			}
@@ -114,11 +156,13 @@ function getBoxOfficeImages() {
 	};
 }
 
+//Function to get movie details by clicking on movie thumbnail on home page
 function getMovieDetailsFromImages(id) {
 	xmlhttp = new XMLHttpRequest();
 	var movieId = id;
 	element = document.getElementById("output2");
 	element.innerHTML = "";
+	//transition to page2
 	$.mobile.changePage($("#page2"), {
 		transition: "slide",
 	});
@@ -126,10 +170,12 @@ function getMovieDetailsFromImages(id) {
 	var url = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieId + ".json?apikey=" + key;
 	xmlhttp.open('GET', url, true);
 	xmlhttp.setRequestHeader("Accept", "application/json");
+	//send the request to RESTful web service
 	xmlhttp.send(null);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
 			if (xmlhttp.status == 200) {
+				//Parsing the json response
 				var movieInfo = JSON.parse(xmlhttp.responseText);
 				var genre = "";
 				var cast = "";
@@ -143,7 +189,15 @@ function getMovieDetailsFromImages(id) {
 				for (i = 0; i < movieInfo.abridged_directors.length; i++) {
 					director = director + movieInfo.abridged_directors[i].name + ", ";
 				}
-				element.innerHTML = element.innerHTML + "<br/>" + "<img src=" + movieInfo.posters.thumbnail + ">" + "<h3>" + movieInfo.title + "(" + movieInfo.year + ")" + "</h3>" + "<b>Genre(s): </b>" + genre + "<br/>" + "<b>Rating: </b>" + movieInfo.mpaa_rating + "<br/>" + "<b>Runtime: </b>" + movieInfo.runtime + " min<br/>" + "<b>In Theaters: </b>" + movieInfo.release_dates.theater + "<br/>" + "<b>Critics Rating: </b>" + movieInfo.ratings.critics_rating + "<br/>" + "<b>Critics Score: </b>" + movieInfo.ratings.critics_score + "<br/>" + "<b>Audience Rating: </b>" + movieInfo.ratings.audience_rating + "<br/>" + "<b>Audience Score: </b>" + movieInfo.ratings.audience_score + "<br/>" + "<b>Summary: </b>" + movieInfo.synopsis + "<br/>" + "<b>Cast: </b>" + cast + "<br/>" + "<b>Director(s): </b>" + director + "<br/>" + "<b>Studio: </b>" + movieInfo.studio + "<br/><br/>" + "<a onclick='getReviews(/" + movieInfo.id + "/,/" + movieInfo.title + "/)' href='#'><b>Reviews</b></a><br/>";
+				element.innerHTML = element.innerHTML + "<br/>" + "<img src=" + movieInfo.posters.thumbnail + ">" 
+				+ "<h3>" + movieInfo.title + "(" + movieInfo.year + ")" + "</h3>" + "<b>Genre(s): </b>" + genre 
+				+ "<br/>" + "<b>Rating: </b>" + movieInfo.mpaa_rating + "<br/>" + "<b>Runtime: </b>" + movieInfo.runtime 
+				+ " min<br/>" + "<b>In Theaters: </b>" + movieInfo.release_dates.theater + "<br/>" + "<b>Critics Rating: </b>" 
+				+ movieInfo.ratings.critics_rating + "<br/>" + "<b>Critics Score: </b>" + movieInfo.ratings.critics_score 
+				+ "<br/>" + "<b>Audience Rating: </b>" + movieInfo.ratings.audience_rating + "<br/>" + "<b>Audience Score: </b>" 
+				+ movieInfo.ratings.audience_score + "<br/>" + "<b>Summary: </b>" + movieInfo.synopsis + "<br/>" + "<b>Cast: </b>" 
+				+ cast + "<br/>" + "<b>Director(s): </b>" + director + "<br/>" + "<b>Studio: </b>" + movieInfo.studio + "<br/><br/>" 
+				+ "<a onclick='getReviews(/" + movieInfo.id + "/,/" + movieInfo.title + "/)' href='#'><b>Reviews</b></a><br/>";
 			} else alert("Error -> xmlhttp status: " + xmlhttp.status);
 		}
 	};
